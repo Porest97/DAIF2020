ca81f3467193ebd1d18bb396ad1b6cb11f438fa7
Per Orest <per.orest@outlook.com>
2020-05-31 00:43:44 +0200

    AlotOfShait

---
+++ SportsLogs/Models/DataModels/SportsLog.cs
@@ -0,0 +1,34 @@
+﻿using DAIF2020.Models.DataModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DAIF2020.SportsLogs.Models.DataModels
+{
+    public class SportsLog
+    {
+        public int Id { get; set; }
+
+        public int? PersonId { get; set; }
+        [Display(Name = "Person")]
+        [ForeignKey("PersonId")]
+        public Person Person { get; set; }
+
+        public int? SportId { get; set; }
+        [Display(Name = "Sport")]
+        [ForeignKey("SportId")]
+        public Sport Sport { get; set; }
+
+        [Display(Name = "Started")]
+        public DateTime? DateTimeStart { get; set; }
+
+        [Display(Name = "Ended")]
+        public DateTime? DateTimeEnd { get; set; }
+
+        [Display(Name ="Time in decimals ")]
+        public decimal TimeSpent { get; set; }
+    }
+}
---
+++ SportsLogs/Models/ViewModels/SPLViewModel.cs
@@ -0,0 +1,15 @@
+﻿using DAIF2020.SportsLogs.Models.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DAIF2020.SportsLogs.Models.ViewModels
+{
+    public class SPLViewModel
+    {
+        public List<SportsLog> SportLogs { get; internal set; }
+
+        public List<Sport> Sports { get; set; }
+    }
+}
